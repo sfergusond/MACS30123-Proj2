e39c7b684dfd400ca121f0c407b68d43c7aebfde
csfergusondryden <55507798+csfergusondryden@users.noreply.github.com>
2020-11-11 09:57:44 -0600

    README

---
+++ README.md
@@ -0,0 +1,20 @@
+## 1
+
+### a) On a t2.micro EC2 instance, the serial version of the code took 149 seconds to run: 12 seconds for step 1, and 137 seconds for step 2. After parallelizing the code, the best result took 34 seconds to run, 7 seconds for step 1, and 27 seconds for step 2. Overall, I saw about a 4x speedup. The largest bottleneck I saw ocurred in step 1. I only noticed a 5 second speedup, so I'm assuming the lambda invocation overhead took a significant amount of time, upwards of 10 seconds. I tested different batch sizes for step 2 and noticed diminishing returns lower/higher than splitting the list of books into ten equal parts. Larger slices meant that each Lamdba task had to compute a lot more in parallel, while smaller slices meant that the parallel benefit became outweighed by the Lambda incovation overhead.
+
+### b) 
+
+## 2
+
+The output for my mrjob implementation: 
+
+[[13064, "the"], [8705, "and"], [7882, "of"], [7018, "a"], [6091, "to"], [4286, "in"], [3135, "is"], [2510, "her"], [2147, "that"], [1996, "with"]]
+
+Runtime: 4.6 seconds
+
+## 3
+
+Note: instead of using boto3 to create the EC2 instances and the Kinesis stream, I just made them manually on the AWS console. I had issues with the IAM permissions when running trying to create those on Python. 
+
+![screenshot]()
+
--- q2_top10words.py
+++ q2_top10words.py
@@ -1,28 +1,39 @@
-from mrjob import MRJob
-import re
+from mrjob.job import MRJob, MRStep
+import re, dataset, time
 
 WORD_RE = re.compile(r"[\w']+")
 
+def getKey(item):
+	return item[0]
+
 class TopTenMRJob(MRJob):
 
-	def configure_args(self):
-		super(TopTenMRJob, self).configure_args()
-		self.add_file_arg('--database')
+	def steps(self):
+		return [
+		    MRStep(mapper=self.mapper_get_words,
+		           combiner=self.combiner_count_words,
+		           reducer=self.reducer_count_words),
+		    MRStep(reducer=self.reducer_find_top_words)
+		]
 
-	def mapper_init(self):
-		# make sqlite3 database available to mapper
-		self.sqlite_conn = sqlite3.connect(self.options.database)
-
-	def mapper(self, _, line):
+	def mapper_get_words(self, _, line):
 		for word in WORD_RE.findall(line):
 		    yield word.lower(), 1
 
-	def combiner(self, word, counts):
+	def combiner_count_words(self, word, counts):
 		yield word, sum(counts)
 
-	def reducer(self, word, counts):
-		yield word, sum(counts)
+	def reducer_count_words(self, word, counts):
+		yield None, (sum(counts), word)
 
+	def reducer_find_top_words(self, _, word_count_pairs):
+		# Sort the key, value pairs by their counts (key) in descending order
+		sorted_word_counts = sorted(list(word_count_pairs), key=getKey, reverse=True)
+
+		# Yield the top ten results
+		yield None, sorted_word_counts[:10]
 
 if __name__ == '__main__':
-    TopTenMRJob.run()
+	t0 = time.time()
+	TopTenMRJob.run()
+	print(time.time()-t0)
